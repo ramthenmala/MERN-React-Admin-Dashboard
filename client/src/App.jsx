2fe0c3c54ffefd64eae72c826139bc77c654c91c
Ram kumar <4328468+ramthenmala@users.noreply.github.com>
2022-12-31 03:13:49 +0000

    mui theme configured

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "./utils/theme";
 
 function App() {
+  const themeMode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [theme]);
 
   return (
     <div className="app">
-      Hi
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { configureStore } from '@reduxjs/toolkit'
-import globalSlice from './state'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { configureStore } from "@reduxjs/toolkit";
+import globalSlice from "./state";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const state = configureStore({
+  reducer: {
+    global: globalSlice,
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider state={state}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
